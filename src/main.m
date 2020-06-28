ac4985c937d7d17aab6cb9ca10e03549d1f65c2e
OnikenX <OnikenX@protonmail.com>
2020-06-28 19:25:00 +0100

    melhores comentarios para o main

--- src/main.m
+++ src/main.m
@@ -1,9 +1,29 @@
+
+%O main é onde se executa a executa a implementação completa do sistema, como já foi dito anteriormente, 
+%não existe interface, deve se editar os ficheiros do programa para se editar os valores pretendidos que não sejam o default.
+
+%%Argumentos
+%Podem ser metidos 2 argumentos opcionais no main
 function  [ out ] = main(mf, valores)
+
+%mf
+%E o primeiro argumento e serve para escolher que função a utilizar
+%Default => gaussmf
+%   1 => trimf
+%   2 ou outro => gaussmf
     warning off
     addpath('./helpers/');
     if ~exist('mf','var')
         mf = 2 ; % 1 - trimf ; 2 - gaussmf
     end
+    
+%valores
+%E o segundo argumento e serve para selecionar os valores de input das várias outras funções, passa o argumento dado para o PreValores
+%Default => faz set aos valores para ter os valores melhores possíveis
+%   1 => o mesmo que o Default
+%   2 => faz set aos valores para ter resultados valores médios
+%   3 => faz set aos valores para ter resultados valores mínimos
+%   4 ou outros => extra para definir e personalizar
     if ~exist('valores', 'var')
         valores = 1;
     end
